36b22807d54eff1bc8e6bcfe6fa3dd0ff780c320
Lucas Pessoa Oliveira Alves <lucaspessoalves25@gmail.com>
2023-05-04 19:32:01 -0300

    Alterando códigos 3d

--- AEDS1/Prática/Cena3d/main.cpp
+++ AEDS1/Prática/Cena3d/main.cpp
@@ -2,15 +2,16 @@
 #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 
 //sqrt = raiz quadrada, pow = potência
 
 int main(int argc, char** argv) {
-
+    cout << fixed << setprecision(2);
     string figura;
-    float area, areatotal, volume, volumetotal, raio, lado, altura;
+    float area, areatotal, volume, volumetotal, raio, lado, lado2, altura;
     ifstream cena ("cena3d.txt");
     if (!cena.is_open()){
         cout << "\nErro: Arquivo não encontrado. \n";
@@ -37,7 +38,7 @@ int main(int argc, char** argv) {
             cout << "O volume do cubo é: "<< area << endl;
             cout << "----------------------------------------------" << endl;
             volumetotal = volume + volumetotal;
-        }else{
+        }else if(figura == "cilindro"){
             cena >> raio;
             cena >> altura;
             area = 2*3.14*raio*(raio+altura);
@@ -47,8 +48,31 @@ int main(int argc, char** argv) {
             cout << "O volume do cilindro é: "<< area << endl;
             cout << "----------------------------------------------" << endl;
             volumetotal = volume + volumetotal;
+        }else if(figura == "paralelepípedo"){
+            cena >> lado;
+            cena >> lado2;
+            cena >> altura;
+            area = 2*(lado*lado2+lado*altura+lado2*altura);
+            cout << "A área do paralelepípedo é: " << area << endl;
+            areatotal = area + areatotal;
+            volume = lado*lado2*altura;
+            cout << "O volume do paralelepípedo é: " << volume << endl;
+            cout << "----------------------------------------------" << endl;
+            volumetotal = volume + volumetotal;
+        }else if(figura == "cone"){
+            cena >> lado;
+            
+            
+            
+            area = areaB + areaL;
+            
+            volume = areaB*alturaP;
+            
+            
+        }else if(figura == "pirâmide_quadrangular"){
+
         }
-        cena >> figura;
+        
     }
     cout << "O volume total da cena é: " << volumetotal<< endl;
     cout << "A área total da cena é: " << areatotal << endl;
@@ -56,4 +80,3 @@ int main(int argc, char** argv) {
     
     return 0;
 }
-
