48567464c72b314c023260028ba7cc79bea1099f
Lucas <lucaspessoalves25@gmail.com>
2023-06-04 11:32:15 -0300

    Exercícios de AEDS1

--- AEDS1/Prática/Lista Vetores/Ex4/main.cpp
+++ AEDS1/Prática/Lista Vetores/Ex4/main.cpp
@@ -7,12 +7,11 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    float vetor1[TAM], vetor2[TAM], maiorvalor, menorvalor;
+    float x, vetor1[TAM], vetor2[TAM], maiorvalor, valor, menorvalor;
     char q;
-    int x, y, cont, newop, posmenor, posmaior;
-    maiorvalor = cont = newop = 0;
+    int y, j, cont, newop, pos, posmenor, posmaior;
+    maiorvalor = cont = newop = j = x = 0;
     menorvalor = MAXFLOAT;
-    
 
     ifstream arquivo("vetores.txt");
     if (!arquivo.is_open())
@@ -25,16 +24,16 @@ int main(int argc, char const *argv[])
     {
         arquivo >> vetor1[i];
     }
-
+    
     while (newop != 6)
     {
         cout << "[1] Encontrar o menor e o maior valor do vetor." << endl;
-        cout << "[2] ." << endl;
-        cout << "[3] " << endl;
-        cout << "[4] Ordenação por placa." << endl;
+        cout << "[2] Procura de um valor no vetor." << endl;
+        cout << "[3] Número de ocorrências de um valor no vetor." << endl;
+        cout << "[4] Se o valor for par, multiplica por 2. Caso contrário atribui 0." << endl;
         cout << "[5] " << endl;
         cout << "[6] Sair do programa." << endl;
-        cout << "Insira a opção desejada:" << endl;
+        cout << "Insira a opção desejada: "; 
         cin >> newop;
         switch (newop)
         {
@@ -52,12 +51,13 @@ int main(int argc, char const *argv[])
                     posmaior = i;
                 }
             }
-            cout << "O menor valor é: " amarelo_N<< menorvalor << reset " na posição: " amarelo_N<< posmenor << endl;
-            cout << reset "O maior valor é: " amarelo_N << maiorvalor << reset " na posição: " amarelo_N << posmaior << reset <<  endl;
+            cout << "O menor valor é: " amarelo_N << menorvalor << reset " na posição: " amarelo_N << posmenor << endl;
+            cout << reset "O maior valor é: " amarelo_N << maiorvalor << reset " na posição: " amarelo_N << posmaior << reset << endl;
 
             cout << "Deseja sair do programa? S/N" << endl;
             cin >> q;
-            while (q != 'n' && q != 'N'){
+            while (q != 'n' && q != 'N')
+            {
                 if (q == 'S' || q == 's')
                 {
                     newop = 6;
@@ -73,8 +73,25 @@ int main(int argc, char const *argv[])
             break;
 
         case 2:
+            cout << "Insira o valor a ser procurado: ";
+            cin >> valor;
 
-            cout << "Deseja sair do programa? S/N" << endl;
+            for (int i = 0; i < TAM; i++)
+            {
+                if (valor == vetor1[i])
+                {
+                    vetor2[j] = i;
+                    j++;
+                }
+            }
+
+            cout << "O valor " amarelo_N << valor << reset " foi encontrado na(s) posição(s): ";
+            for (int i = 0; i < j; i++)
+            {
+                cout << amarelo_N << vetor2[i] << " " reset;
+            }
+
+            cout << "\nDeseja sair do programa? S/N" << endl;
             cin >> q;
             while (q != 'n' && q != 'N')
             {
@@ -93,33 +110,117 @@ int main(int argc, char const *argv[])
             break;
 
         case 3:
+            cout << "Insira o valor a ser procurado: ";
+            cin >> valor;
+
+            for (int i = 0; i < TAM; i++)
+            {
+                if (valor == vetor1[i])
+                {
+                    j++;
+                }
+            }
+
+            cout << "O valor " amarelo_N << valor << reset " ocorre " amarelo_N << j << reset " vezes no vetor." << endl;
+
+            cout << "Deseja sair do programa? S/N" << endl;
+            cin >> q;
+            while (q != 'n' && q != 'N')
+            {
+                if (q == 's' || q == 'S')
+                {
+                    newop = 6;
+                    break;
+                }
+                else
+                {
+                    cout << "Opção inválida." << endl;
+                    cout << "Deseja sair do programa? S/N" << endl;
+                    cin >> q;
+                }
+            }
+
             break;
         case 4:
+            for (int i = 0; i < TAM; i++)
+            {
+                if ((int)vetor1[i] % 2 == 0)
+                {
+                    vetor2[i] = vetor1[i] * 2;
+                }
+                else
+                {
+                    vetor2[i] = 0;
+                }
+            }
+            cout << "Vetor final: " amarelo_N " [ ";
+            for (int i = 0; i < TAM - 1; i++)
+            {
+                cout << vetor2[i] << ", ";
+            }
+            cout << vetor2[TAM - 1] << "] " reset << endl;
+
+
+            cout << "Deseja sair do programa? S/N" << endl;
+            cin >> q;
+            while (q != 'n' && q != 'N')
+            {
+                if (q == 's' || q == 'S')
+                {
+                    newop = 6;
+                    break;
+                }
+                else
+                {
+                    cout << "Opção inválida." << endl;
+                    cout << "Deseja sair do programa? S/N" << endl;
+                    cin >> q;
+                }
+            }
+
             break;
         case 5:
+            cout << "Insira o fator de incrementação: ";
+            cin >> x;
+
+            for(int i = 0; i < TAM; i++){
+                vetor2[i] = vetor1[i] + x;
+                if(vetor2[i] < 0){
+                    vetor2[i] = 0;
+                }else if (vetor2[i] > 255){
+                    vetor2[i] = 255;
+                }
+            }
+            cout << "Caso o valor final seja maior que 255, limita-se em 255." << endl;
+            cout << "Caso o valor final seja menor que 0, limita-se em 0." << endl;
+
+            cout << "Vetor final: " amarelo_N " [ ";
+            for (int i = 0; i < TAM - 1; i++)
+            {
+                cout << vetor2[i] << ", ";
+            }
+            cout << vetor2[TAM - 1] << "] " reset << endl;
+
+            cout << "Deseja sair do programa? S/N" << endl;
+            cin >> q;
+            while (q != 'n' && q != 'N')
+            {
+                if (q == 's' || q == 'S')
+                {
+                    newop = 6;
+                    break;
+                }
+                else
+                {
+                    cout << "Opção inválida." << endl;
+                    cout << "Deseja sair do programa? S/N" << endl;
+                    cin >> q;
+                }
+            }
+
             break;
         default:
             cout << vermelho_G "Programa finalizado." reset << endl;
         }
-
-        for (int i = 0; i < TAM; i++)
-        {
-            if (vetor1[i] > x && vetor1[i] < y)
-            {
-                vetor2[cont] = vetor1[i];
-                cont++;
-            }
-        }
     }
-
-    /*cout << "Vetor final: " magenta_N "[";
-    for (int i = 0; i < cont - 1; i++)
-    {
-        cout << vetor2[i] << ", ";
-    }
-    cout << vetor2[cont - 1] << "] " reset << endl;*/
-
-    return 0;
 }
-
-
