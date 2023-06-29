5f305a076d72f99fa4455d39779e935f86e18832
Lucas Pessoa Oliveira Alves <lucaspessoalves25@gmail.com>
2023-06-29 19:08:26 -0300

    Funções do trabalho

--- AEDS1/Prática/Trabalho 2/main.cpp
+++ AEDS1/Prática/Trabalho 2/main.cpp
@@ -7,11 +7,12 @@ int bubblesort(int v[], int n){
   aux = contBubble = 0;
   for (int i = n - 1; i > 0; i--){
     for (int j = 0; j < i; j++){
-        if (v[j] > v[j+1]){
-            aux = v[j];
-            v[j] = v[j+1];
-            v[j+1] = aux;
-        }
+      if (v[j] > v[j + 1]){
+        aux = v[j];
+        v[j] = v[j + 1];
+        v[j + 1] = aux;
+        contBubble = contBubble + 4;
+      }
     }
   }
   return 0;
@@ -20,36 +21,36 @@ int bubblesort(int v[], int n){
 int insertionsort(int v[], int n){
   int aux, j, contInsertion;
   contInsertion = 0;
-    for (int i = 1; i < n; i++){
-        aux = v[i];
-        j = i - 1;
-        while (j >= 0 && v[j] > aux){
-            v[j+1] = v[j];
-            j--;
-            contInsertion++;
-        }
-        v[j+1] = aux;
+  for (int i = 1; i < n; i++){
+    aux = v[i];
+    j = i - 1;
+    while (j >= 0 && v[j] > aux){
+      v[j + 1] = v[j];
+      j--;
+      contInsertion++;
     }
-    return contInsertion;
+    v[j + 1] = aux;
+  }
+  return contInsertion;
 }
 
 int selectionsort(int v[], int n){
   int aux, min, contSelection;
   contSelection = 0;
-    for (int i = 0; i < n; i++){
-        min = i;
-        aux = v[i];
-        for(int j = i+1; j < n; j++){
-            if (v[j] < aux){
-                aux = v[j];
-                min = j;
-            }
-        }
-        v[min] = v[i];
-        v[i] = aux;
-        contSelection++;
+  for (int i = 0; i < n; i++){
+    min = i;
+    aux = v[i];
+    for (int j = i + 1; j < n; j++){
+      if (v[j] < aux){
+        aux = v[j];
+        min = j;
+      }
     }
-    return contSelection;
+    v[min] = v[i];
+    v[i] = aux;
+    contSelection++;
+  }
+  return contSelection;
 }
 
 void imprimevetor(int v[], int n){
@@ -60,28 +61,57 @@ void imprimevetor(int v[], int n){
 
 int main(int argc, char const *argv[]){
 
-  int contSelection;
-  int vb0[10] = {0,1,2,3,4,5,6,7,8,9}; // Vetor ordenado.
-  int vb1[10] = {9,8,2,0,6,5,1,4,3,7}; // Vetor desordenado.
-  int vb2[10] = {9,8,7,6,5,4,3,2,1,0}; // Vetor em ordem inversa.
+  int contador;
+  int i, m, n, aux, qtd;
+  bool existe;
+  i = m = n = aux = qtd = 0;
 
- /* cout << "Selection Sort, vetor ordenado: " << endl;
-  imprimevetor(vb0, 10);
-  contSelection = selectionsort(vb0, 10);
-  imprimevetor(vb0, 10);
-  cout << "Contagem: " << contSelection << endl;
+  ofstream out;
+  out.open("random.txt");
+  srand(time(NULL));
 
-  cout << "Selection Sort, vetor desordenado: " << endl;
-  imprimevetor(vb1, 10);
-  contSelection = selectionsort(vb1, 10);
-  imprimevetor(vb1, 10);
-  cout << "Contagem: " << contSelection << endl;
+  cout << "Insira a quantidade de elementos do vetor: ";
+  cin >> n;
+  cout << "Insira o valor máximo do intervalo: ";
+  cin >> m;
+  int vetor1[n];
 
-  cout << "Selection Sort, vetor em ordem inversa: " << endl;
-  imprimevetor(vb2, 10);
-  contSelection = selectionsort(vb2, 10);
-  imprimevetor(vb2, 10);
-  cout << "Contagem: " << contSelection << endl;*/
+  while (i < n){
+    vetor1[i] = rand() % m;
+    existe = false;
+    for (int j = 0; j < i; j++){
+      if (vetor1[j] == vetor1[i]){
+        existe = true;
+      }
+    }
+    if (!existe){
+      i++;
+    }
+  }
+
+  out << n << endl;
+  for (int i = 0; i < n - 1; i++){
+    out << vetor1[i] << endl;
+  }
+  out << vetor1[n - 1];
+
+  /* cout << "Selection Sort, vetor ordenado: " << endl;
+   imprimevetor(vb0, 10);
+   contSelection = selectionsort(vb0, 10);
+   imprimevetor(vb0, 10);
+   cout << "Contagem: " << contSelection << endl;
+
+   cout << "Selection Sort, vetor desordenado: " << endl;
+   imprimevetor(vb1, 10);
+   contSelection = selectionsort(vb1, 10);
+   imprimevetor(vb1, 10);
+   cout << "Contagem: " << contSelection << endl;
+
+   cout << "Selection Sort, vetor em ordem inversa: " << endl;
+   imprimevetor(vb2, 10);
+   contSelection = selectionsort(vb2, 10);
+   imprimevetor(vb2, 10);
+   cout << "Contagem: " << contSelection << endl;*/
 
   /*cout << " Buble Sort, vetor ordenado: " << endl;
   imprimevetor(vb0, 10);
@@ -98,25 +128,32 @@ int main(int argc, char const *argv[]){
   bubblesort(vb2, 10);
   imprimevetor(vb2, 10);*/
 
-    cout << " Insertion Sort, vetor ordenado: " << endl;
+  /*cout << "Insertion Sort, vetor ordenado: " << endl;
   imprimevetor(vb0, 10);
   contSelection = insertionsort(vb0, 10);
   imprimevetor(vb0, 10);
-  cout << "Contagem: " << contSelection << endl;
+  cout << "Contagem: " << contSelection << endl;*/
 
-  cout << " Insertion Sort, vetor desordenado: " << endl;
-  imprimevetor(vb1, 10);
-  contSelection = insertionsort(vb1, 10);
-  imprimevetor(vb1, 10);
-  cout << "Contagem: " << contSelection << endl;
+  
+  cout << "Contagem no Insertion com vetor aleatório: ";
+  contador = insertionsort(vetor1, n);
+  cout << contador << endl;
 
 
-  cout << " Insertion Sort, vetor em ordem inversa: " << endl;
+  cout << "Contagem no insertion com vetor crescente: ";
+  contador = insertionsort(vetor1, n);
+  cout << contador << endl;
+
+
+  cout << "Contagem no insertion com vetor decrescente: ";
+  contador = insertionsort(vetor1, n);
+  cout << contador << endl;
+
+  /*cout << "Insertion Sort, vetor em ordem inversa: " << endl;
   imprimevetor(vb2, 10);
   contSelection = insertionsort(vb2, 10);
   imprimevetor(vb2, 10);
-  cout << "Contagem: " << contSelection << endl;
+  cout << "Contagem: " << contSelection << endl;*/
 
-
-    return 0;
+  return 0;
 }
