21c892bbf05aa01cd2bdb4c81f538c2f37b6cff8
unknown <lucaspessoalves25@gmail.com>
2023-07-04 11:49:04 -0300

    AEDS1

--- AEDS1/Prática/Trabalho 2/main.cpp
+++ AEDS1/Prática/Trabalho 2/main.cpp
@@ -45,6 +45,7 @@ int insertionsort(int vetor[], int n){
     v[j + 1] = aux;
     contInsertion += 3;
   }
+
   return contInsertion;
 }
 
@@ -143,82 +144,130 @@ void open_file(int vetor[], int n){
       cout << "Erro na abertura do arquivo" << endl;
       exit(1);
     }
-  arquivo >> valor; //descarte da primeira linha d o arquivo
+  arquivo >> valor; //descarte da primeira linha do arquivo
   for (int i = 0; i < n ; i++){
     arquivo >> valor;
     vetor[i] = valor;
   }
+  arquivo.close();
 }
 
+void header_exit_file(string filename){
+  ofstream out (filename);
+  if (!out.is_open()){
+    cout << "Erro na criação do arquivo" << endl;
+    exit(1);
+  }
+  out << "qtd_elementos,cont_bbs,cont_ins,cont_sel\n" << endl;
+  out.close(); 
+}
 
+void exit_file(string filename, int qtd_elementos, int cont_bbs, int cont_ins, int cont_sel){
+  ofstream out;
+  out.open(filename, ios_base::app);
+  if (!out.is_open()){
+    cout << "Não foi possível abrir o arquivo de saída: " << filename << endl;
+    exit(1);
+  }
+  out << qtd_elementos << "," << cont_bbs << "," << cont_ins << "," << cont_sel << endl;
+  out.close(); 
+}
+
+void interface(){
+  cout << " __   __  _______  _______  _______  ______   _______  _______                    " << endl;
+  cout << "|  |_|  ||       ||       ||       ||      | |       ||       |                   " << endl;
+  cout << "|       ||    ___||_     _||   _   ||  _    ||   _   ||  _____|                   " << endl;
+  cout << "|       ||   |___   |   |  |  | |  || | |   ||  | |  || |_____                    " << endl;
+  cout << "|       ||    ___|  |   |  |  |_|  || |_|   ||  |_|  ||_____  |                   " << endl;
+  cout << "| ||_|| ||   |___   |   |  |       ||       ||       | _____| |                   " << endl;
+  cout << "|_|   |_||_______|  |___|  |_______||______| |_______||_______|                   " << endl;
+  cout << " ______   _______                                                                 " << endl;
+  cout << "|      | |       |                                                                " << endl;
+  cout << "|  _    ||    ___|                                                                " << endl;
+  cout << "| | |   ||   |___                                                                 " << endl;
+  cout << "| |_|   ||    ___|                                                                " << endl;
+  cout << "|       ||   |___                                                                 " << endl;
+  cout << "|______| |_______|                                                                " << endl;
+  cout << " _______  ______    ______   _______  __    _  _______  _______  _______  _______ " << endl;
+  cout << "|       ||    _ |  |      | |       ||  |  | ||   _   ||       ||   _   ||       |" << endl;
+  cout << "|   _   ||   | ||  |  _    ||    ___||   |_| ||  |_|  ||       ||  |_|  ||   _   |" << endl;
+  cout << "|  | |  ||   |_||_ | | |   ||   |___ |       ||       ||       ||       ||  | |  |" << endl;
+  cout << "|  |_|  ||    __  || |_|   ||    ___||  _    ||       ||      _||       ||  |_|  |" << endl;
+  cout << "|       ||   |  | ||       ||   |___ | | |   ||   _   ||     |_ |   _   ||       |" << endl;
+  cout << "|_______||___|  |_||______| |_______||_|  |__||__| |__||_______||__| |__||_______|" << endl;                                                  
+  cout << "==================================================================================" << endl;
+}
+
+void result_print(int qtd_elementos, int cont_bbs, int cont_ins, int cont_sel){
+  
+}
+
+void result_to_file(int vetor[], int intervalo, int n){
+  int cont_bbs, cont_ins, cont_sel;
+  cont_bbs = cont_ins = cont_sel = 0;
+  for (int i = 1; i <= n; i += intervalo){
+    cont_bbs = bubblesort(vetor, i);
+    cont_ins = insertionsort(vetor, i);
+    cont_sel = selectionsort(vetor, i);
+    exit_file("ALEATORIO.txt", i, cont_bbs, cont_ins, cont_sel );
+
+    ordena_vetor(vetor, i, "cresc");
+    cont_bbs = bubblesort(vetor, i);
+    cont_ins = insertionsort(vetor, i);
+    cont_sel = selectionsort(vetor, i);
+    exit_file("CRESCENTE.txt", i, cont_bbs, cont_ins, cont_sel );
+
+    ordena_vetor(vetor, i, "dec");
+    cont_bbs = bubblesort(vetor, i);
+    ordena_vetor(vetor, i, "dec");
+    cont_ins = insertionsort(vetor, i);
+    ordena_vetor(vetor, i, "dec");
+    cont_sel = selectionsort(vetor, i);
+    exit_file("DECRESCENTE.txt", i, cont_bbs, cont_ins, cont_sel );
+  }
+}
 int main(int argc, char const *argv[]){
 
-  int contador;
-  int m, intervalo, n, aux, qtd;
-  intervalo = m = n = aux = qtd = 0;
+  int m, intervalo, n, aux, qtd, cont_sel, cont_ins, cont_bbs, cont_total_bbs, cont_total_ins, cont_total_sel;
+  cont_total_sel = cont_total_ins = cont_total_bbs = cont_sel = cont_bbs = cont_ins = intervalo = m = n = aux = qtd = 0;
+
+  interface();
 
   cout << "Insira a quantidade de elementos do vetor: ";
   cin >> n;
-  cout << "Insira o valor maximo do intervalo: ";
+  int vetor[n] = {};
+
+  cout << "Insira o valor maximo: ";
   cin >> m;
   while (m <= n){
     cout << "Valor maximo menor que a quantidade de elementos" << endl;
     cout << "Insira outro valor maximo: ";
     cin >> m;
   }
+
   cout << "Insira o intervalo de comparação: ";
   cin >> intervalo;
-  cout << "------------------------------------------" << endl;
-  int vetor[n] = {};
+  while (intervalo >= n){
+    cout << "Itervalo maior que a quantidade de elementos" << endl;
+    cout << "Insira um intervalo válido: ";
+    cin >> intervalo;
+  }
+
+  cout << "==================================================================================" << endl;
   
   create_file_random(n, m);
   open_file(vetor, n);
-  cout << "------------------------------------------" << endl;
+  header_exit_file("ALEATORIO.txt");
+  header_exit_file("CRESCENTE.txt");
+  header_exit_file("DECRESCENTE.txt");
+  cout << "==================================================================================" << endl;
 
-  cout << "Contagem no metodo InsertionSort com vetor aleatorio: ";
-  contador = insertionsort(vetor, n);
-  cout << contador << endl;
-
-  cout << "Contagem no metodo BubbleSort com vetor aleatorio: ";
-  contador = bubblesort(vetor, n);
-  cout << contador << endl;
-
-  cout << "Contagem no metodo SelectionSort com vetor aleatorio: ";
-  contador = selectionsort(vetor, n);
-  cout << contador << endl;
-  cout << "------------------------------------------" << endl;
+  cont_total_bbs = bubblesort (vetor, n);
+  cont_total_ins = insertionsort (vetor, n);
+  cont_total_sel = selectionsort (vetor, n);
 
 
-  ordena_vetor(vetor, n, "cresc");
-  cout << "Contagem no metodo InsertionSort com vetor crescente: ";
-  contador = insertionsort(vetor, n);
-  cout << contador << endl;
-  
-  cout << "Contagem no metodo BubbleSort com vetor crescente: ";
-  contador = bubblesort(vetor, n);
-  cout << contador << endl;
-
-  cout << "Contagem no metodo SelectionSort com vetor crescente: ";
-  contador = selectionsort(vetor, n);
-  cout << contador << endl;  
-  cout << "------------------------------------------" << endl;
-
-
-  ordena_vetor(vetor, n, "dec");
-  cout << "Contagem no metodo InsertionSort com vetor decrescente: ";
-  contador = insertionsort(vetor, n);
-  cout << contador << endl;
-
-  ordena_vetor(vetor, n, "dec");
-  cout << "Contagem no metodo BubbleSort com vetor decrescente: ";
-  contador = bubblesort(vetor, n);
-  cout << contador << endl;
-
-  ordena_vetor(vetor, n, "dec");
-  cout << "Contagem no metodo SelectionSort com vetor decrescente: ";
-  contador = selectionsort(vetor, n);
-  cout << contador << endl;
-  cout << "------------------------------------------" << endl;
+  result_to_file(vetor, intervalo, n);
 
 
   return 0;
