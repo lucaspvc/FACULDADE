db255decfdea39277ec1fb748f11a0cbda556f4b
Lucas Pessoa Oliveira Alves <lucapessoalves25@gmail.com>
2023-10-23 15:41:08 -0300

    aula de probabilidade

---
+++ Estatística/AulaDeProbabilidade/main.r
@@ -0,0 +1,11 @@
+# Bibliotecas básicas para utilizar o R no Visual Studio Code
+library(datasets)
+library(base)
+library(graphics)
+library(grDevices)
+library(methods)
+library(stats)
+library(utils)
+library(readxl)
+library(RcmdrMisc)
+
