a9bf02e334e6df081830ee7169ad924ce7c38cf5
Lucas Pessoa Oliveira Alves <lucaspessoalves25@gmail.com>
2023-11-21 10:11:07 -0300

    Compiladores

--- Compiladores/3-geracao/utils.c
+++ Compiladores/3-geracao/utils.c
@@ -19,8 +19,7 @@ int posTab = 0; // indica a proxima posicao livre para inserir
 int buscaSimbolo(char *s)
 {
     int i;
-    for (i = posTab - 1; strcmp(tabSimb[i].id, s) && i >= 0; i--)
-        ;
+    for (i = posTab - 1; strcmp(tabSimb[i].id, s) && i >= 0; i--);
     if (i == -1)
     {
         char msg[200];
@@ -35,11 +34,9 @@ void insereSimbolo(struct elemTabSimbolos elem)
     int i;
     if (posTab == TAM_TAB)
         yyerror("Tabela de Simbolos cheia");
-    for (i = posTab - 1; strcmp(tabSimb[i].id, elem.id) && i >= 0; i--)
-        ;
+    for (i = posTab - 1; strcmp(tabSimb[i].id, elem.id) && i >= 0; i--);
     if (i != -1)
     {
-        char msg[200];
         sprintf(msg, "Identificador [%s] duplicado!", elem.id);
         yyerror(msg);
     }
