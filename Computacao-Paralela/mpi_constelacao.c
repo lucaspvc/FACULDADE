ea0a07cc22c7ceceb7325539d6807c5a23798959
Lucas Pessoa <lucaspessoalves25@gmail.com>
2024-09-19 17:26:11 -0300

    trabalho-constelacao

---
+++ Computacao-Paralela/mpi_constelacao.c
@@ -0,0 +1,18 @@
+/**
+Exercício
+Encontrar a quantidade de estrelas (grupo de pixels branco) numa imagem celeste (imagem preto). A imagem terá 
+formato PGM com valores entre 0 e 255m e deve ser lido pelo processo 0. Para processar em paralelo, a imagem deve ser 
+dividido em partes iguais na vertical e na horizontal. As estrelas possuem pixels com valores menores do que
+20 em tons de cinza.
+
+
+
+ 0  1  2  3  4
+ 5  6  7  8  9
+ 10 11 12 13 14
+ 15 16 17 18 19
+ 20 21 22 23 24
+
+
+
+ */
